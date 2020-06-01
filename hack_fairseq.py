4b6ed01c18f82b5aa96b92e32edf6c74d7983592
zhouyi <dugu9sword@163.com>
2020-06-02 04:02:41 +0800

    backup

--- hack_fairseq.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def use_fairseq_9():
-    if os.path.exists("fairseq"):
-        os.rename("fairseq", "fairseq0.6")
-
-def use_fairseq_6():
-    if os.path.exists("fairseq0.6"):
-        os.rename("fairseq0.6", "fairseq")
