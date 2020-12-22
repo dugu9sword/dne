c28dfed4a6b22014c9f46abefef83b93e2d24e49
zhouyi <dugu9sword@163.com>
2020-12-22 18:05:35 +0800

    backup

--- pipeline.sh
+++
@@ -1,4 +0,0 @@
-rm -rf train.logs/
-rm -rf attack.logs/
-alchemist --task=train
-alchemist --task=attack
--- show_train.sh
+++
@@ -1,7 +0,0 @@
-#num=$(ls -l | grep task | wc -l)
-num=$(grep "START TASK" $1/alchemist.txt | wc -l)
-#num=$(echo $num-1|bc)
-for (( i=0; i<$num; ++i )); do
-  cat $1/task-${i}.txt | grep accuracy | tail -n 1 
-#  echo ""
-done
